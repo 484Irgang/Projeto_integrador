2507ef7a938ff49a5d05e1c67c03cef699f79e45
Guilherme Irgang <484gui@gmail.com>
2023-11-04 22:52:14 -0300

    --append

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $host = $_ENV['MYSQLHOST'];
 $password = $_ENV['MYSQL_ROOT_PASSWORD'];
 
 try {
-  $db = new PDO('mysql:host='.$host.';dbname=railway', 'root', $password);
+  $db = new PDO('mysql:host=viaduct.proxy.rlwy.net;dbname=railway', 'root', $password);
   echo 'Banco de dados acessado com sucesso';
 } catch (PDOException $e){
   echo $e;
