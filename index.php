d9f59547bd921052722cac0a2463a8d5fafd81eb
Guilherme Irgang <484gui@gmail.com>
2023-11-04 23:02:00 -0300

    --append

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
-$host = $_ENV['MYSQLHOST'];
-$password = $_ENV['MYSQL_ROOT_PASSWORD'];
-
-try {
-  $db = new PDO('mysql:host=viaduct.proxy.rlwy.net;dbname=railway', 'root', $password);
-  echo 'Banco de dados acessado com sucesso';
-} catch (PDOException $e){
-  echo $e;
+function onConnectDB() {
+  $password = $_ENV['MYSQL_ROOT_PASSWORD'];
+  try {
+    $db = new PDO('mysql:host=viaduct.proxy.rlwy.net;dbname=railway', 'root', $password);
+    echo 'Banco de dados acessado com sucesso';
+  } catch (PDOException $e){
+    echo $e;
+  }
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <button onclick="onConnectDB()">Chamar</button>
+</body>
+</html>
