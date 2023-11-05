28a80b0dd63f9566ac56ad433888e7f48af2cfe3
Guilherme Irgang <484gui@gmail.com>
2023-11-04 22:44:58 -0300

    --append

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-$host = $_ENV['DB_HOST'];
-$password = $_ENV['DB_PASSWORD'];
+$host = $_ENV['MYSQLHOST'];
+$password = $_ENV['MYSQL_ROOT_PASSWORD'];
 echo $host." = ".$password;
 
-try {
-  $db = new PDO('mysql:host='.$host.';dbname=railway', 'root', $password);
-  echo 'Banco de dados acessado com sucesso';
-} catch (PDOException $e){
-  echo $e;
-}
+// try {
+//   $db = new PDO('mysql:host='.$host.';dbname=railway', 'root', $password);
+//   echo 'Banco de dados acessado com sucesso';
+// } catch (PDOException $e){
+//   echo $e;
+// }
