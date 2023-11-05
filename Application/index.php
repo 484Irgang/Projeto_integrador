a6b50b5a9086508f2e5673079cb3d30ad67aacab
Guilherme Irgang <484gui@gmail.com>
2023-11-05 14:35:04 -0300

    --append

--- Application/index.php
+++ Application/index.php
@@ -6,15 +6,24 @@
 
   function getPDO() {
     try{
-      $db = new PDO('mysql:host=viaduct.proxy.rlwy.net;dbname=railway;charset=utf8mb4;port=17472', 'root', 'CBac1AEAcchg3-1eCFHd64Ec3ba1a65H');
+      $host = $_ENV['MYSQLHOST'];
+      $database = $_ENV['MYSQLDATABASE'];
+      $user = $_ENV['MYSQLUSER'];
+      $port = $_ENV['MYSQLPORT'];
+      $password = $_ENV['MYSQL_ROOT_PASSWORD'];
+
+
+      $db = new PDO('mysql:host='.$host.';dbname='.$database.';charset=utf8mb4;port='.$port, $user, $password);
       $info = $db->query("SELECT * from student");
       print_r($info);
-      echo 'conected';
+      echo '<br> Banco de dados conectado <br>';
     } catch( PDOException $e) {
       print_r($e);
     }
   }
   getPDO();
 
+  echo '<pre>';
   print_r($_ENV);
+  echo '</pre>';
 ?>
