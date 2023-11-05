9f0b05d4ab4015b7fa09df7b12c674c8e2f05d37
Guilherme Irgang <484gui@gmail.com>
2023-11-04 22:30:53 -0300

    test: data base test

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-
-echo 'Olá mundo';
+$host = $_ENV['DB_HOST'];
+$password = $_ENV['DB_PASSWORD'];
+echo $host." = ".$password;
+try {
+  $db = new PDO('mysql:host='.$host.';dbname=railway', 'root', $password);
+  echo 'Banco de dados acessado com sucesso'
+} catch (PDOException $e){
+  echo $e
+}
