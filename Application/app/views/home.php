3f7d79820da468668a84ba032e5cb841ac27bf21
Guilherme Irgang <484gui@gmail.com>
2023-11-12 00:30:31 -0300

    feat: create layout to home and login pages

--- Application/app/views/home.php
+++ Application/app/views/home.php
@@ -1,3 +1,20 @@
 <section id="home_page">
-  <h2>Home</h2>
+  <div class="create_accounts">
+    <a href="/user/create/ADMIN"><button>
+      <div class="admin_icon"></div>
+      <p>Cadastrar Administrador</p>
+    </button></a>
+    <a href="/user/create/STUDENT"><button>
+      <div class="student_icon"></div>
+      <p>Cadastrar Estudante</p>
+    </button></a>
+    <a href="/user/create/TEACHER"><button>
+    <div class="teacher_icon"></div>
+      <p>Cadastrar Professor</p>
+    </button></a>
+    <a href="/user/create/SUPPLIER"><button>
+    <div class="supplier_icon"></div>
+      <p>Cadastrar Fornecedor</p>
+    </button></a>
+  </div>
 </section>
--- Application/app/views/login.php
+++ Application/app/views/login.php
@@ -1,9 +1,19 @@
 <section id="section_login">
+  <h1>Bem vindo</h1>
+  <div id="login_content">
   <h2>Login</h2>
   <form method="post" action="/login">
-    <input type="email" name="email" required/>
-    <input type="password" name="password" required />
+    <div class="field_box">
+      <p>email:</p>
+      <input type="email" name="email" required/>
+    </div>
+    
+    <div class="field_box">
+      <p>senha:</p>
+      <input type="password" name="password" required />
+    </div>
 
     <button type="submit">Entrar</button>
   </form>
+  </div>
 </section>
