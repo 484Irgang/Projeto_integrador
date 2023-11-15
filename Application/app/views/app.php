0903d776f513bb5070b27b137af8c8568310dd81
lazariniuser <gabriellazarinicontato@gmail.com>
2023-11-15 19:10:22 -0300

    tela dinamica

--- Application/app/views/app.php
+++ Application/app/views/app.php
@@ -5,6 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $title?></title>
   <link rel="stylesheet" type="text/css" href="/assets/styles/app.css" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   <?php 
 
     echo '<link rel="stylesheet" type="text/css" href="/assets/styles/'.$style_file.'" />'
--- Application/app/views/users_list.php
+++ Application/app/views/users_list.php
@@ -1,14 +1,4 @@
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <title>Encontrar pessoas</title>
-</head>
-<body class="bg-dark">
-
 <div class="container mt-5">
     <h2 class="mb-4 text-light text-center">Eles estão com a gente!</h2>
 
@@ -40,11 +30,3 @@
 
     </div>
 </div>
-
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-</body>
-</html>
